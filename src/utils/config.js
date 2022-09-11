06e8da04d88355a8f509b60c9f49b566f219a71e
Antonio Semeraro <antsemeraro@gmail.com>
2022-09-11 23:37:11 +0200

    Tokens USD value
    Shows tokens USD value using Kaddex price API

--- src/utils/config.js
+++ src/utils/config.js
@@ -16,6 +16,7 @@ export const CONFIG = {
 };
 export const NUMBER_DECIMAL_AFTER_DOT = 12;
 export const ESTIMATE_KDA_TO_USD_API = 'https://api.coingecko.com/api/v3/simple/price?vs_currencies=usd&ids=kadena,';
+export const KADDEX_ANALYTICS_API = 'https://analytics-api.kaddex.com/candles';
 export const TELEGRAM_GROUP_LINK = 'https://t.me/kadena_io';
 export const WEBSITE_LINK = 'https://kaddex.com/';
 export const TERM_LINK = 'https://xwallet.kaddex.com/terms-of-use';
