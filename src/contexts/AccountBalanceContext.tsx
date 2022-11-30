fd71825caeacbc2dedbaaba72519c39d9777d1e1
Antonio Semeraro <antsemeraro@gmail.com>
2022-11-30 22:30:24 +0100

    Testnet balances fix

--- src/contexts/AccountBalanceContext.tsx
+++ src/contexts/AccountBalanceContext.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-syntax */
 import { useState, createContext, useEffect, useContext } from 'react';
 import { useSelector } from 'react-redux';
 import moment from 'moment';
@@ -5,7 +6,7 @@ import useLocalStorage from 'src/hooks/useLocalStorage';
 import { useInterval } from 'src/hooks/useInterval';
 import { IFungibleToken, LOCAL_KEY_FUNGIBLE_TOKENS } from 'src/pages/ImportToken';
 import { useCurrentWallet } from 'src/stores/wallet/hooks';
-import { fetchListLocal, fetchTokenList } from 'src/utils/chainweb';
+import { fetchListLocal, fetchTokenList, MAINNET_NETWORK_ID } from 'src/utils/chainweb';
 import { KADDEX_ANALYTICS_API } from 'src/utils/config';
 import { CHAIN_COUNT } from 'src/utils/constant';
 import { SettingsContext } from './SettingsContext';
@@ -51,37 +52,67 @@ export const AccountBalanceProvider = ({ children }: any) => {
   const uniqueWallets = wallets.map((w) => w.account).filter((value, index, self) => self.indexOf(value) === index);
 
   const fetchAllBalances = async (account: string) => {
-    const promiseList: any[] = [];
+    if (selectedNetwork.networkId === MAINNET_NETWORK_ID) {
+      const promiseList: any[] = [];
 
-    for (let i = 0; i < CHAIN_COUNT; i += 1) {
-      const availableChainTokens = allChainAvailableTokens && allChainAvailableTokens[i];
-      const filteredAvailableFt = fungibleTokens?.filter((t) => availableChainTokens?.includes(t.contractAddress));
-      const pactCode = `
-      (
-        let* (
-              (coin-balance (try 0.0 (coin.get-balance "${account}")))
-              ${filteredAvailableFt
-                ?.map((ft) => `(${ft.contractAddress.replace(/\./g, '')} (try 0.0 (${ft.contractAddress}.get-balance "${account}")))`)
-                .join('\n')}              
-             )
-        {"coin": coin-balance, ${filteredAvailableFt?.map((ft) => `"${ft.contractAddress}": ${ft.contractAddress.replace(/\./g, '')}`)}}
-      )`;
-      const promise = fetchListLocal(
-        pactCode,
-        selectedNetwork.url,
-        selectedNetwork.networkId,
-        i.toString(),
-        txSettings?.gasPrice,
-        txSettings?.gasLimit,
-      );
-      promiseList.push(promise);
+      for (let i = 0; i < CHAIN_COUNT; i += 1) {
+        const availableChainTokens = allChainAvailableTokens && allChainAvailableTokens[i];
+        const filteredAvailableFt = fungibleTokens?.filter((t) => availableChainTokens?.includes(t.contractAddress));
+        const pactCode = `
+        (
+          let* (
+                (coin-balance (try 0.0 (coin.get-balance "${account}")))
+                ${filteredAvailableFt
+                  ?.map((ft) => `(${ft.contractAddress.replace(/\./g, '')} (try 0.0 (${ft.contractAddress}.get-balance "${account}")))`)
+                  .join('\n')}              
+               )
+          {"coin": coin-balance, ${filteredAvailableFt?.map((ft) => `"${ft.contractAddress}": ${ft.contractAddress.replace(/\./g, '')}`)}}
+        )`;
+        const promise = fetchListLocal(
+          pactCode,
+          selectedNetwork.url,
+          selectedNetwork.networkId,
+          i.toString(),
+          txSettings?.gasPrice,
+          txSettings?.gasLimit,
+        );
+        promiseList.push(promise);
+      }
+      return Promise.all(promiseList).then((allRes) => {
+        setAccountBalanceState((prev) => ({
+          ...prev,
+          [account]: allRes.map((chainBalance) => chainBalance?.result?.data),
+        }));
+      });
     }
-    return Promise.all(promiseList).then((allRes) => {
-      setAccountBalanceState((prev) => ({
-        ...prev,
-        [account]: allRes.map((chainBalance) => chainBalance?.result?.data),
-      }));
-    });
+    const fts: IFungibleToken[] = [
+      { contractAddress: 'coin', symbol: 'KDA' },
+      { contractAddress: 'kaddex.kdx', symbol: 'KDX' },
+      ...(fungibleTokens || []),
+    ];
+    const chainBalance: TokenBalance[] = [];
+    for (let i = 0; i < CHAIN_COUNT; i += 1) {
+      const tokenBalance: TokenBalance = {};
+      for (const ft of fts) {
+        const pactCode = `(${ft.contractAddress}.get-balance "${account}")`;
+        // eslint-disable-next-line no-await-in-loop
+        const pactResponse = await fetchListLocal(
+          pactCode,
+          selectedNetwork.url,
+          selectedNetwork.networkId,
+          i.toString(),
+          txSettings?.gasPrice,
+          txSettings?.gasLimit,
+        );
+        tokenBalance[ft.contractAddress] = pactResponse?.result?.data || 0;
+      }
+      chainBalance.push(tokenBalance);
+    }
+
+    return setAccountBalanceState((prev) => ({
+      ...prev,
+      [account]: chainBalance,
+    }));
   };
 
   const updateUsdPrices = () => {
@@ -129,7 +160,9 @@ export const AccountBalanceProvider = ({ children }: any) => {
       const sortedWallets = uniqueWallets.sort((a, b) => b.indexOf(selectedAccount));
       const promises: any = [];
       for (let i = 0; i < sortedWallets.length; i += 1) {
-        promises.push(fetchAllBalances(sortedWallets[i]));
+        if (selectedNetwork.networkId === MAINNET_NETWORK_ID || i === 0) {
+          promises.push(fetchAllBalances(sortedWallets[i]));
+        }
       }
       Promise.all(promises).then(() => setIsLoadingBalances(false));
     }
