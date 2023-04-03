0eff30257eee803d040c008215c79607e1dfc221
Antonio Semeraro <antsemeraro@gmail.com>
2023-04-03 20:07:54 +0200

    v2.4.1

--- src/pages/Setting/index.tsx
+++ src/pages/Setting/index.tsx
@@ -142,7 +142,7 @@ const PageSetting = () => {
       {settingsMenu.map((menuItem) => !menuItem.isHidden && getSettingsItem(menuItem))}
       <AboutDiv marginTop="48px" alignItems="center">
         <SecondaryLabel fontWeight={500}>
-          eckoWALLET V. 2.4.0
+          eckoWALLET V. 2.4.1
           <br />
           <br />
           The Kadena ecosystem gateway
