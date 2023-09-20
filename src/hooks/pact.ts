c07d0c3f107d5f5f25775292b140d5e0bde8d34c
Mattia Alfonso <alfonsomattia@gmail.com>
2023-09-20 16:59:49 +0200

    Improved pact hooks

--- src/hooks/pact.ts
+++ src/hooks/pact.ts
@@ -3,18 +3,23 @@ import { useAppSelector } from 'src/stores/hooks';
 import { useCurrentWallet } from 'src/stores/wallet/hooks';
 import { fetchLocal } from 'src/utils/chainweb';
 
-export const useExecPact = <Response = any>(pactCode: string, chainId: string) => {
+export const useExecPact = <Response = any>(chainId: string) => {
   const selectedNetwork = useAppSelector(getSelectedNetwork);
 
-  return async () => {
+  return async (pactCode: string) => {
     const response = await fetchLocal(pactCode, selectedNetwork.url, selectedNetwork.networkId, chainId);
     return response.result as Response;
   };
+};
+
+export const useExecPreparedPact = <Response = any>(pactCode: string, chainId: string) => {
+  const execPact = useExecPact<Response>(chainId);
+  return async () => execPact(pactCode);
 };
 
 export const useExecPactWithLocalAccount = <Response = any>(pactCode: string, chainId: string) => {
   const { account } = useCurrentWallet();
   pactCode = pactCode.replace('{{ACCOUNT}}', account);
 
-  return useExecPact<Response>(pactCode, chainId);
+  return useExecPreparedPact<Response>(pactCode, chainId);
 };
