3d0026bd30c7e9d9aa152d664b523aac3b3a3d5a
Mattia Alfonso <alfonsomattia@gmail.com>
2023-10-07 15:30:29 +0200

    ConfirmAmountView refactor

--- src/components/GovernanceMining/Stake/ConfirmAmountView.tsx
+++ src/components/GovernanceMining/Stake/ConfirmAmountView.tsx
@@ -1,63 +1,24 @@
 import React from 'react';
-import styled from 'styled-components';
-import { TokenElement } from 'src/pages/Wallet/components/TokenElement';
-import { DivFlex, SecondaryLabel } from 'src/components';
-import Button from 'src/components/Buttons';
-import images from 'src/images';
+import BaseConfirmAmount from './BaseConfirmAmount';
 
-interface ConfirmAmountViewProps {
+export interface ConfirmAmountViewProps {
   amount: number;
   usdAmount: number;
   isLoading: boolean;
   onConfirm: () => void;
 }
 
-const Subtitle = styled(SecondaryLabel)`
-  text-transform: uppercase;
-  color: #A0A6AA;
-  font-weight: bold;
-`;
-
-const Description = styled(SecondaryLabel)`
-  font-weight: 300;
-  font-size: 14px;
-  margin-bottom: 8px;
-`;
-
-const Separator = styled.div`
-  margin: 8px 0px;
-  border-bottom: 1px solid ${({ theme }) => theme.border};
-`;
-
-const ConfirmAmount = ({ amount, usdAmount, isLoading, onConfirm }: ConfirmAmountViewProps) => (
-  <DivFlex flexDirection="column" padding="24px" paddingTop="0px" gap="8px">
-    <Subtitle>
-      Adding more KDX to your staking amount
-    </Subtitle>
-
-    <Description>
+const ConfirmAmount = (props: ConfirmAmountViewProps) => (
+  <BaseConfirmAmount
+    {...props}
+    summaryTitle="Adding more KDX to your staking amount"
+    summaryDescription={`
       Adding more KDX to your stake, will simply increase your waiting time proportionally,
       and it will also activate a 3% flat penalty fee on any unstaked amount for 72hours.
       Once the 72 hours have passed, you can unstake your tokens normally without paying this penalty.
-    </Description>
-
-    <Separator />
-
-    <Subtitle>
-      Stake
-    </Subtitle>
-
-    <TokenElement
-      name="KDX"
-      balance={amount}
-      usdBalance={usdAmount}
-      logo={images.wallet.tokens['kaddex.kdx']}
-    />
-
-    <DivFlex justifyContent="center" padding="4px" paddingTop="8px">
-      <Button isDisabled={isLoading} label={isLoading ? '...' : 'Confirm'} size="full" variant="primary" onClick={onConfirm} />
-    </DivFlex>
-  </DivFlex>
+    `}
+    title="Stake"
+  />
 );
 
 export default ConfirmAmount;
--- src/components/GovernanceMining/Unstake/ConfirmAmountView.tsx
+++ src/components/GovernanceMining/Unstake/ConfirmAmountView.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
-import { TokenElement } from 'src/pages/Wallet/components/TokenElement';
-import { DivFlex, SecondaryLabel } from 'src/components';
-import Button from 'src/components/Buttons';
+import { SecondaryLabel } from 'src/components';
 import { Radio } from 'src/components/Radio';
-import images from 'src/images';
 import {
   Container as DetailsContainer,
   Key as DetailsKey,
   Value as DetailsValue,
   StatusValue as DetailsStatusValue,
 } from '../Details/UI';
+import BaseConfirmAmount from '../Stake/BaseConfirmAmount';
 
 interface ConfirmAmountViewProps {
   amount: number;
@@ -25,36 +23,33 @@ interface ConfirmAmountViewProps {
   onConfirm: (withdraw: boolean) => void;
 }
 
-const Subtitle = styled(SecondaryLabel)`
-  text-transform: uppercase;
-  color: #A0A6AA;
-  fontWeight: bold;
-`;
-
 const Description = styled(SecondaryLabel)`
   font-weight: 300;
   font-size: 14px;
   margin-bottom: 8px;
 `;
 
-const Separator = styled.div`
-  margin: 8px 0px;
-  border-bottom: 1px solid ${({ theme }) => theme.border};
-`;
-
 const ConfirmAmount = ({
-  amount,
   hasPositionPenalty,
   positionPenalty,
   positionPenaltyPercentage,
   hasRewardPenalty,
   rewardPenalty,
   rewardPenaltyPercentage,
-  usdAmount,
-  isLoading = false,
   onConfirm,
+  ...props
 }: ConfirmAmountViewProps) => {
   const [withdraw, setWithdraw] = React.useState(false);
+
+  const summaryTitle = `Closing your staking plan ${hasPositionPenalty ? 'early' : ''}`;
+  const summaryDescription = (hasPositionPenalty) ? (
+    'Removing KDX from your staking amount will incur a penalty of 3% of the unstaked amount.' +
+    'All the KDX penalties are going to be burnt in order to reduce the overall supply.'
+  ) : (
+    'Are you sure you want to close your staking plan? Partially or completely removing your staking position ' +
+    'will have a negative effect on your Voting power.'
+  );
+
   const toggleWithdraw = () => {
     setWithdraw((value) => !value);
   };
@@ -63,53 +58,27 @@ const ConfirmAmount = ({
     onConfirm(withdraw);
   };
 
-  return (
+  const summaryDetails = (
+    <DetailsContainer>
+      {(hasPositionPenalty) && (
+        <>
+          <DetailsKey>Position penalty</DetailsKey>
+          <DetailsValue>{positionPenaltyPercentage}%</DetailsValue>
+        </>
+      )}
+      {(withdraw && hasRewardPenalty) && (
+        <>
+          <DetailsKey>Rewards penalty</DetailsKey>
+          <DetailsValue>{rewardPenaltyPercentage}%</DetailsValue>
+        </>
+      )}
+      <DetailsKey>Amount</DetailsKey>
+      <DetailsValue>{props.amount} KDX</DetailsValue>
+    </DetailsContainer>
+  );
 
-    <DivFlex flexDirection="column" padding="24px" paddingTop="0px" gap="8px">
-      <Subtitle>
-        Closing your staking plan {hasPositionPenalty ? 'early' : ''}
-      </Subtitle>
-
-      <Description>
-        {(hasPositionPenalty) ? (
-          'Removing KDX from your staking amount will incur a penalty of 3% of the unstaked amount.' +
-          'All the KDX penalties are going to be burnt in order to reduce the overall supply.'
-        ) : (
-          'Are you sure you want to close your staking plan? Partially or completely removing your staking position ' +
-          'will have a negative effect on your Voting power.'
-        )}
-      </Description>
-
-      <DetailsContainer>
-        {(hasPositionPenalty) && (
-          <>
-            <DetailsKey>Position penalty</DetailsKey>
-            <DetailsValue>{positionPenaltyPercentage}%</DetailsValue>
-          </>
-        )}
-        {(withdraw && hasRewardPenalty) && (
-          <>
-            <DetailsKey>Rewards penalty</DetailsKey>
-            <DetailsValue>{rewardPenaltyPercentage}%</DetailsValue>
-          </>
-        )}
-        <DetailsKey>Amount</DetailsKey>
-        <DetailsValue>{amount} KDX</DetailsValue>
-      </DetailsContainer>
-
-      <Separator />
-
-      <Subtitle>
-        Unstaked amount
-      </Subtitle>
-
-      <TokenElement
-        name="KDX"
-        balance={amount}
-        usdBalance={usdAmount}
-        logo={images.wallet.tokens['kaddex.kdx']}
-      />
-
+  const details = (
+    <>
       <DetailsContainer>
         {hasPositionPenalty && (
           <>
@@ -124,7 +93,6 @@ const ConfirmAmount = ({
           </>
         )}
       </DetailsContainer>
-
       <Radio
         onClick={toggleWithdraw}
         isChecked={withdraw}
@@ -134,11 +102,19 @@ const ConfirmAmount = ({
           </Description>
         }
       />
+    </>
+  );
 
-      <DivFlex justifyContent="center" padding="4px" paddingTop="8px">
-        <Button isDisabled={isLoading} label={isLoading ? '...' : 'Confirm'} size="full" variant="primary" onClick={handleConfirm} />
-      </DivFlex>
-    </DivFlex>
+  return (
+    <BaseConfirmAmount
+      {...props}
+      summaryTitle={summaryTitle}
+      summaryDescription={summaryDescription}
+      summaryDetails={summaryDetails}
+      title="Unstaked amount"
+      details={details}
+      onConfirm={handleConfirm}
+    />
   );
 };
 
