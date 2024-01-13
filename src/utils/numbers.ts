2b62b1c945fa4022c40b177bcf6fba6c58ea7898
Mattia Alfonso <alfonsomattia@gmail.com>
2024-01-13 22:42:12 +0100

    Fixed precision when sent amount is represented with scientific notation

--- src/utils/numbers.ts
+++ src/utils/numbers.ts
@@ -1,6 +1,16 @@
-export function getFloatPrecision(floatVal) {
-  if (!floatVal || floatVal.toString().indexOf('.') < 0) {
-    return 0;
+export function getFloatPrecision(floatVal: number) {
+  if (!floatVal) return 0;
+
+  const asString = floatVal.toString();
+  const hasScientificNotation = asString.includes('e-');
+  const separatorIndex = asString.indexOf('.');
+
+  if (!hasScientificNotation && separatorIndex < 0) return 0;
+
+  if (hasScientificNotation) {
+    const exponent = asString.split('e-')[1];
+    return +exponent;
   }
-  return Math.abs(floatVal.toString().indexOf('.') - floatVal.toString().length + 1);
+
+  return Math.abs(separatorIndex - asString.length + 1);
 }
