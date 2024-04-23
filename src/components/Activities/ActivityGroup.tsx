178827aaa54ff3d410aecf024c0b19d50561aefe
Mattia Alfonso <alfonsomattia@gmail.com>
2024-04-24 01:06:50 +0200

    Local activities sorted

--- src/components/Activities/ActivityGroup.tsx
+++ src/components/Activities/ActivityGroup.tsx
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import moment from 'moment';
 import { SecondaryLabel } from 'src/components';
 import FinishTransferItem from 'src/components/Activities/FinishTransferItem';
 import { LocalActivity } from './types';
@@ -16,12 +15,6 @@ const DayLabel = styled(SecondaryLabel)`
   font-weight: 700;
 `;
 
-const compareByCreatedTime = (a: LocalActivity, b: LocalActivity) => {
-  const timeA = moment(a.createdTime, 'ddd MMM DD YYYY HH:mm:ss ZZ');
-  const timeB = moment(b.createdTime, 'ddd MMM DD YYYY HH:mm:ss ZZ');
-  return timeB.unix() - timeA.unix();
-};
-
 interface Props {
   label: string;
   activities: LocalActivity[];
@@ -37,7 +30,7 @@ const ActivityGroup = ({
 }: Props) => (
   <>
     <DayLabel uppercase>{label}</DayLabel>
-    {activities.sort(compareByCreatedTime).map((item) => {
+    {activities.map((item) => {
       if (!item || !item.receiverChainId) return null;
       return (
         <Div key={`${item.requestKey}${item.direction}${item.sender}${item.receiver}`} style={{ padding: '0px 24px' }} onClick={() => openActivityDetail(item)}>
--- src/components/Activities/List.tsx
+++ src/components/Activities/List.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import styled from 'styled-components';
+import { groupBy, orderBy } from 'lodash';
 import moment from 'moment';
-import { groupBy } from 'lodash';
 import { DivFlex, SecondaryLabel } from 'src/components';
 import ActivityGroup from './ActivityGroup';
 import Filters from './Filters';
@@ -35,27 +35,47 @@ const List = ({
   const [status, setStatus] = useState<StatusValue>();
   const [token, setToken] = useState<string>();
 
-  const filteredActivitiesByStatus = status ? activities.filter(
-    (activity) => {
-      switch (status) {
-        case 'IN':
-          return activity.direction === 'IN';
-        case 'OUT':
-          return activity.direction === 'OUT';
-        case 'PENDING':
-          return activity.status === 'pending';
-        default:
-          return true;
-      }
-    },
-  ) : activities;
-  const filteredActivities = token ? filteredActivitiesByStatus.filter(
-    (activity) => activity.module === token,
-  ) : filteredActivitiesByStatus;
+  const sorted = useMemo(() => {
+    const localActivities = activities || [];
+    const filteredActivitiesByStatus = status ? localActivities.filter(
+      (activity) => {
+        switch (status) {
+          case 'IN':
+            return activity.direction === 'IN';
+          case 'OUT':
+            return activity.direction === 'OUT';
+          case 'PENDING':
+            return activity.status === 'pending';
+          default:
+            return true;
+        }
+      },
+    ) : localActivities;
+    const filteredActivities = token ? filteredActivitiesByStatus.filter(
+      (activity) => activity.module === token,
+    ) : filteredActivitiesByStatus;
 
-  const grouped = groupBy(filteredActivities, (activity) => moment(new Date(activity.createdTime)).format('DD/MM/YYYY'));
+    const groupedActivities = groupBy(filteredActivities, (activity) => moment(new Date(activity.createdTime)).format('DD/MM/YYYY'));
+
+    const sortedActivities = Object.keys(groupedActivities).reduce((acc, key) => {
+      acc[key] = orderBy(groupedActivities[key], (activity) => moment(new Date(activity.createdTime)).unix(), 'desc');
+      return acc;
+    }, {} as Record<string, LocalActivity[]>);
+
+    const sortedKeys = Object.keys(groupedActivities).sort((a, b) => moment(b, 'DD/MM/YYYY').unix() - moment(a, 'DD/MM/YYYY').unix());
+    const withSortedKeys = sortedKeys.reduce((acc, key) => {
+      acc.set(key, sortedActivities[key]);
+      return acc;
+    }, new Map<string, LocalActivity[]>());
+
+    return withSortedKeys;
+  }, [activities, status, token]);
+
   const todayString = moment().format('DD/MM/YYYY');
+  const todayActivities = sorted.get(todayString);
   const yesterdayString = moment().subtract(1, 'days').format('DD/MM/YYYY');
+  const yesterdayActivities = sorted.get(yesterdayString);
+  const keys = [...sorted.keys()];
 
   return (
     <Div>
@@ -65,34 +85,34 @@ const List = ({
         token={token}
         onChangeToken={setToken}
       />
-      {Object.keys(grouped)?.length ? (
+      {keys.length ? (
         <DivChild>
           <DivScroll>
-            {grouped && grouped[todayString] && (
+            {todayActivities && (
               <ActivityGroup
                 label="Today"
-                activities={grouped[todayString]}
+                activities={todayActivities}
                 pendingCrossChainRequestKeys={pendingCrossChainRequestKeys}
                 openActivityDetail={openActivityDetail}
               />
             )}
 
-            {grouped && grouped[yesterdayString] && (
+            {yesterdayActivities && (
               <ActivityGroup
                 label="Yesterday"
-                activities={grouped[yesterdayString]}
+                activities={yesterdayActivities}
                 pendingCrossChainRequestKeys={pendingCrossChainRequestKeys}
                 openActivityDetail={openActivityDetail}
               />
             )}
 
-            {Object.keys(grouped)
+            {keys
               .filter((key) => key !== yesterdayString && key !== todayString)
               .map((date) => (
                 <ActivityGroup
                   key={date}
                   label={moment(date, 'DD/MM/YYYY').format('DD/MM/YYYY')}
-                  activities={grouped[date]}
+                  activities={sorted.get(date) || []}
                   pendingCrossChainRequestKeys={pendingCrossChainRequestKeys}
                   openActivityDetail={openActivityDetail}
                 />
